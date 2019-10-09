a2a5aff7d0c9c56d17691ab333c994eb81bfa430
Rizqi Mauludin <rizqimauludin10@gmail.com>
2019-10-09 14:04:25 +0700

    Home UI (50%)

--- app/src/main/java/com/example/rumahkite/MainActivity.java
+++ app/src/main/java/com/example/rumahkite/MainActivity.java
@@ -9,8 +9,16 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.navigation.NavigationView;
@@ -28,6 +36,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
 
+        //ImageView imageView = (ImageView) findViewById(R.id.rekomenimage);
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +50,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         collapsingToolbarLayout.setTitle("");
         collapsingToolbarLayout.setCollapsedTitleTextColor(ContextCompat.getColor(this,R.color.white));
         collapsingToolbarLayout.setExpandedTitleColor(ContextCompat.getColor(this,R.color.white));
+
+        /*Bitmap mbitmap = ((BitmapDrawable) getResources().getDrawable(R.drawable.rekomendasi)).getBitmap();
+        Bitmap imageRounded = Bitmap.createBitmap(mbitmap.getWidth(), mbitmap.getHeight(), mbitmap.getConfig());
+        Canvas canvas = new Canvas(imageRounded);
+        Paint mpaint = new Paint();
+        mpaint.setAntiAlias(true);
+        mpaint.setShader(new BitmapShader(mbitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+        canvas.drawRoundRect((new RectF(0, 0, mbitmap.getWidth(), mbitmap.getHeight())), 100, 100, mpaint);// Round Image Corner 100 100 100 100
+        imageView.setImageBitmap(imageRounded);*/
     }
 
     @Override
