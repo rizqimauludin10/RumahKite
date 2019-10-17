3db5b2a29c9c289bfdf56c1f45eea26ddaa6cb36
Rizqi Mauludin <rizqimauludin10@gmail.com>
2019-10-18 03:37:13 +0700

    Atok

--- app/src/main/java/com/example/rumahkite/ListHomeActivity.java
+++ app/src/main/java/com/example/rumahkite/ListHomeActivity.java
@@ -6,6 +6,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.rumahkite.adapter.ListHomeAdapter;
+import com.example.rumahkite.model.Home;
 
 import com.example.rumahkite.adapter.ListHomeAdapter;
 import com.example.rumahkite.model.Home;
@@ -40,17 +44,30 @@ public class ListHomeActivity extends AppCompatActivity {
         listHomeAdapter.setOnItemClickCallBack(new ListHomeAdapter.OnItemClickCallBack() {
             @Override
             public void onItemClicked(Home data) {
+                //showSelectedHome(data);
                 Intent intent = new Intent(ListHomeActivity.this, DetailActivity.class);
                 intent.putExtra(DetailActivity.EXTRA_TYPE, data.getType_home());
                 intent.putExtra(DetailActivity.EXTRA_PRICE, data.getHarga());
                 intent.putExtra(DetailActivity.EXTRA_DESC, data.getDeskripsi());
                 intent.putExtra(DetailActivity.EXTRA_PIC, data.getFoto());
+                intent.putExtra(DetailActivity.EXTRA_KEC, data.getKecamatan());
+                intent.putExtra(DetailActivity.EXTRA_SALE, data.getmSale());
+                intent.putExtra(DetailActivity.EXTRA_AGENT, data.getNamaAgent());
+                intent.putExtra(DetailActivity.EXTRA_BED, data.getkSleep());
+                intent.putExtra(DetailActivity.EXTRA_BATH, data.getkBath());
+                intent.putExtra(DetailActivity.EXTRA_LANDSIZE, data.getLandsize());
+                intent.putExtra(DetailActivity.EXTRA_CERTIFICATE, data.getTipeSertifikat());
+                intent.putExtra(DetailActivity.EXTRA_ELECTRY, data.getElectric());
+                intent.putExtra(DetailActivity.EXTRA_GARDEN, data.getGarden());
+                intent.putExtra(DetailActivity.EXTRA_SINCE, data.getSince());
+                intent.putExtra(DetailActivity.EXTRA_FLOOR, data.getFloor());
+
                 startActivity(intent);
             }
         });
     }
 
 //    private void showSelectedHome(Home home) {
-//        Toast.makeText(this, "Kau Menyentuh " + home.getType_home(),Toast.LENGTH_LONG).show();
+//        Toast.makeText(this, "Kau Menyentuh " + home.getType_home(), Toast.LENGTH_LONG).show();
 //    }
 }
--- app/src/main/java/com/example/rumahkite/fragment/Form3Fragment.java
+++ app/src/main/java/com/example/rumahkite/fragment/Form3Fragment.java
@@ -1,9 +1,12 @@
 package com.example.rumahkite.fragment;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,11 +17,24 @@ import com.stepstone.stepper.Step;
 import com.stepstone.stepper.VerificationError;
 
 public class Form3Fragment extends Fragment implements Step {
+    View v;
+    private EditText EtAlamat;
+    private TextView TvAlamat;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.form3_fragment, container, false);
+        v =  inflater.inflate(R.layout.form3_fragment, container, false);
+
+        EtAlamat = v.findViewById(R.id.etAlamat);
+        TvAlamat = v.findViewById(R.id.tvAlamat);
+
+        Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "font/TitilliumWeb-SemiBold.ttf");
+        Typeface font2 = Typeface.createFromAsset(getActivity().getAssets(), "font/TitilliumWeb-Regular.ttf");
+        TvAlamat.setTypeface(font);
+        EtAlamat.setTypeface(font2);
+
+        return v;
     }
 
     @Nullable
